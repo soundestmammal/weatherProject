fd4851e1980a7943fb34a468a888549dc0d5c443
soundestmammal <robert.checco@gmail.com>
2017-04-28 17:46:09 -0400

    Add weatherapi key and function

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+var API_KEY = "1c36c1bc4095e967d00414e9c7409d67";
+
+
 $(function(){
 
 	var loc;
 
-	$.getJSON('https://ipinfo.io', function(d){
+	$.getJSON('http://ipinfo.io', function(d){
   console.log("Assigning the data ...")
-  data = d.loc;
+  loc = d.loc.split(",");
+  console.log(loc);
   
-
+  $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' 
+  	+ loc[0] + '&lon=' loc[1] + '&APPID=' + API_KEY, function(wd){
+  		console.log("got the data ," , wd);
+  	});
   //call to the weather API
+
 });
 
 
