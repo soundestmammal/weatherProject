95ec2d459bc859991f120f531c2c96de6c03ea9a
soundestmammal <robert.checco@gmail.com>
2017-04-28 14:20:53 -0400

    Add html, css,  and js

---
+++ app.js
@@ -0,0 +1,15 @@
+$(function(){
+
+	var loc;
+
+	$.getJSON('https://ipinfo.io', function(d){
+  console.log("Assigning the data ...")
+  data = d.loc;
+  
+
+  //call to the weather API
+});
+
+
+});
+//Get the lat and lon
