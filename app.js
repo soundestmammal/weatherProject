d3c5258da67c22476e3fb46ff2af080dc7bd52e1
soundestmammal <robert.checco@gmail.com>
2017-04-29 01:28:06 -0400

    Add toggle feature

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ var API_KEY = "1c36c1bc4095e967d00414e9c7409d67";
 var cel = false;
 var wd;
 
+	function displayTemp(fTemp , c){
+		if (c) return Math.round((fTemp - 32) * (5/9)) + " C";
+		return Math.round(fTemp) + " F";
+	};
+
+function render(wd){
+	var currentLocation = wd.name;
+   	var currentWeather = wd.weather[0].description;
+   	var currentTemp = displayTemp(wd.main.temp , cel);
+   	var high = displayTemp(wd.main.temp_max , cel);
+   	var low = displayTemp(wd.main.temp_min , cel);
+   	var icon = wd.weather[0].icon;
+
+
+
+   	$('#currentLocation').html(currentLocation);
+   	$('#currentWeather').html(currentWeather);
+   	$('#currentTemp').html(currentTemp);
+   	$('#high-low').html(high + " / " + low);
+
+
+   	var iconSrc = 'http://openweathermap.org/img/w/' + icon + ".png";
+   	// console.log(iconSrc);
+   	$("#currentTemp").prepend("<img src='" + iconSrc + "'>");
+	}
+
 $(function(){
 
 	var loc;
@@ -15,24 +41,33 @@ $(function(){
    	+ loc[0] + '&lon=' + loc[1] + '&APPID=' + API_KEY, function(apiData){
    		wd = apiData;
    	console.log("got the data ," , wd);
-   	var currentLocation = wd.name;
-   	var currentWeather = wd.weather[0].description;
-   	var currentTemp = wd.main.temp;
-   	var high = wd.main.temp_max;
-   	var low = wd.main.temp_min;
-   	var icon = wd.weather[0].icon;
+
+   	render(apiData , cel);
+
+   	$("#toggle").click(function(){
+   		cel = !cel;
+   		render(wd , cel);
+   	});
+
+
+   	// var currentLocation = wd.name;
+   	// var currentWeather = wd.weather[0].description;
+   	// var currentTemp = displayTemp(wd.main.temp , cel);
+   	// var high = displayTemp(wd.main.temp_max , cel);
+   	// var low = displayTemp(wd.main.temp_min , cel);
+   	// var icon = wd.weather[0].icon;
 
 
 
-   	$('#currentLocation').html(currentLocation);
-   	$('#currentWeather').html(currentWeather);
-   	$('#currentTemp').html(Math.round(currentTemp)  + " Degrees (F)");
-   	$('#high-low').html(Math.round(high) + " / " + Math.round(low));
+   	// $('#currentLocation').html(currentLocation);
+   	// $('#currentWeather').html(currentWeather);
+   	// $('#currentTemp').html(currentTemp);
+   	// $('#high-low').html(high + " / " + low);
 
 
-   	var iconSrc = 'http://openweathermap.org/img/w/' + icon + ".png";
-   	// console.log(iconSrc);
-   	$("#currentTemp").prepend("<img src='" + iconSrc + "'>");
+   	// var iconSrc = 'http://openweathermap.org/img/w/' + icon + ".png";
+   	// // console.log(iconSrc);
+   	// $("#currentTemp").prepend("<img src='" + iconSrc + "'>");
    })
   
 
